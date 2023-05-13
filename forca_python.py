5391a2a9d8f4d5fe68c429d20cc6c8973fa541e2
ArthurSous4 <arthur28lucas@gmail.com>
2023-05-12 23:47:06 -0300

    Adicionando mais features

--- forca_python.py
+++ forca_python.py
@@ -1,51 +1,56 @@
-import choice from random
+import random
 
-PalavraSelecionada = []
-PalavraDividida = []
-placar = []
-AnimaisSecretas = ["leao", "urso", "rato", "girafa", "tigre", "foca", "baleia", "jacare", "leopardo"]
+resp="sim"
+while resp=="sim":
 
-PalavraSelecionada.append(choice(AnimaisSecretas))
+    PalavraSelecionada = []
+    PalavraDividida = []
+    placar = []
+    AnimaisSecretas = ["leao", "urso", "rato", "girafa", "tigre", "foca", "baleia", "jacare", "leopardo"]
 
-for letra in PalavraSelecionada[0]:
-    PalavraDividida.append(letra)
+    PalavraSelecionada.append(random.choice(AnimaisSecretas))
 
-contLetra = 0
+    for letra in PalavraSelecionada[0]:
+        PalavraDividida.append(letra)
 
-for espaco in PalavraDividida:
-    contLetra = contLetra + 1
-    placar.append("_")
+    contLetra = 0
 
-print("****" * 20)
-print("                              JOGO DA FORCA")
-print("****" * 20)
-print("")
-print(f"A palavra tem: {contLetra} letras\n{placar}")
+    for espaco in PalavraDividida:
+        contLetra = contLetra + 1
+        placar.append("_")
 
-while True:
-    chute = input("\nDigite uma letra ou digite 'chutar' para adivinhar a palavra: ").lower()
+    print("****" * 20)
+    print("                              JOGO DA FORCA")
+    print("****" * 20)
+    print("")
+    print(f"A palavra tem: {contLetra} letras\n{placar}")
 
-    if chute == "chutar":
-        palavra_chute = input("Digite a palavra completa: ").lower()
+    while True:
+        chute = input("\nDigite uma letra ou digite 'chutar' para adivinhar a palavra: ").lower()
 
-        if palavra_chute == PalavraSelecionada[0]:
-            print("Parabéns! Você acertou a palavra!")
-            break
+        if chute == "chutar":
+            palavra_chute = input("Digite a palavra completa: ").lower()
+
+            if palavra_chute == PalavraSelecionada[0]:
+                print("Parabéns! Você acertou a palavra!")
+                break
+            else:
+                print("Você errou a palavra!")
+
+        elif len(chute) == 1:
+            acertou = False
+
+            for i, letra in enumerate(PalavraDividida):
+                if chute == letra:
+                    placar[i] = letra
+                    acertou = True
+
+            if acertou:
+                print(f"Letra '{chute}' está na palavra!\n{placar}")
+            else:
+                print(f"Letra '{chute}' não está na palavra!\n{placar}")
+
         else:
-            print("Você errou a palavra!")
+            print("Entrada inválida! Digite apenas uma letra ou 'chutar' para adivinhar a palavra.")
 
-    elif len(chute) == 1:
-        acertou = False
-
-        for i, letra in enumerate(PalavraDividida):
-            if chute == letra:
-                placar[i] = letra
-                acertou = True
-
-        if acertou:
-            print(f"Letra '{chute}' está na palavra!\n{placar}")
-        else:
-            print(f"Letra '{chute}' não está na palavra!\n{placar}")
-
-    else:
-        print("Entrada inválida! Digite apenas uma letra ou 'chutar' para adivinhar a palavra.")
+    resp=str(input("Quer jogar novamente?")).lower()
