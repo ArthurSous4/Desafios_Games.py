587673325bb876007675bcdd303f86d70c7cff74
ArthurSous4 <arthur28lucas@gmail.com>
2023-05-26 23:39:51 -0300

    Funcionalidades adicionadas:
    -Issue: "Não dar velha resolvido"
    -Implementação do sistema de "jogar novamente"

--- Jogo_da_Velha.py
+++ Jogo_da_Velha.py
@@ -6,8 +6,6 @@ class JogoDaVelha:
             '7': ' ', '8': ' ', '9': ' '
         }
 
-#formato do tabuleiro em dicionário
-
     def mostrar_tabuleiro(self):
         print("┌───┬───┬───┐")
         print(f"│ {self.tabuleiro['1']} │ {self.tabuleiro['2']} │ {self.tabuleiro['3']} │")
@@ -17,117 +15,90 @@ class JogoDaVelha:
         print(f"│ {self.tabuleiro['7']} │ {self.tabuleiro['8']} │ {self.tabuleiro['9']} │")
         print("└───┴───┴───┘")
 
-#mostrar o placar do jogo
-
-jogo=JogoDaVelha()
-jogadorAtual="X"
-
-print(f'''
-░░░░░██╗░█████╗░░██████╗░░█████╗░░░░░░░░░██████╗░░█████╗░░░░░░░░░██╗░░░██╗███████╗██╗░░░░░██╗░░██╗░█████╗░
-░░░░░██║██╔══██╗██╔════╝░██╔══██╗░░░░░░░░██╔══██╗██╔══██╗░░░░░░░░██║░░░██║██╔════╝██║░░░░░██║░░██║██╔══██╗
-░░░░░██║██║░░██║██║░░██╗░██║░░██║░░░░░░░░██║░░██║███████║░░░░░░░░╚██╗░██╔╝█████╗░░██║░░░░░███████║███████║
-██╗░░██║██║░░██║██║░░╚██╗██║░░██║░░░░░░░░██║░░██║██╔══██║░░░░░░░░░╚████╔╝░██╔══╝░░██║░░░░░██╔══██║██╔══██║
-╚█████╔╝╚█████╔╝╚██████╔╝╚█████╔╝░░░░░░░░██████╔╝██║░░██║░░░░░░░░░░╚██╔╝░░███████╗███████╗██║░░██║██║░░██║
-░╚════╝░░╚════╝░░╚═════╝░░╚════╝░░░░░░░░░╚═════╝░╚═╝░░╚═╝░░░░░░░░░░░╚═╝░░░╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝''')
-
-#Processo pra verificação da resposta
-while True:
-
-    jogadorAtual=str(input("Qual jogador vai começar?('X' ou 'O')\n")).upper()
-
-    if jogadorAtual != "X" and jogadorAtual != "O":
-        print("Valor inválido, digite 'X' ou 'O' ")
-
-    else:
-        print(f"Jogador Selecionado: {jogadorAtual}")
-        placar = JogoDaVelha()
-        placar.mostrar_tabuleiro()
-        break
-
-#Primeira jogada
-
-jogada=int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
-
-while jogada < 1 or jogada > 9 or placar.tabuleiro[str(jogada)] != ' ':
-    if jogada < 1 or jogada > 9:
-        print("Escolha Inválida")
-        jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
-
-    else:
-        print("Slot Já Prenchido")
-        jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
-
-jogo.tabuleiro[str(jogada)] = jogadorAtual
-jogo.mostrar_tabuleiro()
-
-if jogadorAtual == "X":
-    jogadorAtual = "O"
-else:
+resp = "sim"
+while resp == "sim":
+    jogo = JogoDaVelha()
     jogadorAtual = "X"
 
-#Proximas jogadas
-turno=2
+    print(f'''
+    ░░░░░██╗░█████╗░░██████╗░░█████╗░░░░░░░░░██████╗░░█████╗░░░░░░░░░██╗░░░██╗███████╗██╗░░░░░██╗░░██╗░█████╗░
+    ░░░░░██║██╔══██╗██╔════╝░██╔══██╗░░░░░░░░██╔══██╗██╔══██╗░░░░░░░░██║░░░██║██╔════╝██║░░░░░██║░░██║██╔══██╗
+    ░░░░░██║██║░░██║██║░░██╗░██║░░██║░░░░░░░░██║░░██║███████║░░░░░░░░╚██╗░██╔╝█████╗░░██║░░░░░███████║███████║
+    ██╗░░██║██║░░██║██║░░╚██╗██║░░██║░░░░░░░░██║░░██║██╔══██║░░░░░░░░░╚████╔╝░██╔══╝░░██║░░░░░██╔══██║██╔══██║
+    ╚█████╔╝╚█████╔╝╚██████╔╝╚█████╔╝░░░░░░░░██████╔╝██║░░██║░░░░░░░░░░╚██╔╝░░███████╗███████╗██║░░██║██║░░██║
+    ░╚════╝░░╚════╝░░╚═════╝░░╚════╝░░░░░░░░░╚═════╝░╚═╝░░╚═╝░░░░░░░░░░░╚═╝░░░╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝''')
 
-for turnos in range (1,9):
+    while True:
+        jogadorAtual = str(input("Qual jogador vai começar?('X' ou 'O')\n")).upper()
+
+        if jogadorAtual != "X" and jogadorAtual != "O":
+            print("Valor inválido, digite 'X' ou 'O' ")
+        else:
+            print(f"Jogador Selecionado: {jogadorAtual}")
+            placar = JogoDaVelha()
+            placar.mostrar_tabuleiro()
+            break
+
     jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
 
     while jogada < 1 or jogada > 9 or placar.tabuleiro[str(jogada)] != ' ':
         if jogada < 1 or jogada > 9:
             print("Escolha Inválida")
             jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
-
         else:
-            print("Slot Já Prenchido")
+            print("Slot Já Preenchido")
             jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
 
     jogo.tabuleiro[str(jogada)] = jogadorAtual
     jogo.mostrar_tabuleiro()
 
-#Condições de vitória Vertical
-    if (jogo.tabuleiro['1'] == jogo.tabuleiro['4'] == jogo.tabuleiro['7'] == jogadorAtual or
-        jogo.tabuleiro['2'] == jogo.tabuleiro['5'] == jogo.tabuleiro['8'] == jogadorAtual or
-        jogo.tabuleiro['3'] == jogo.tabuleiro['6'] == jogo.tabuleiro['9'] == jogadorAtual):
-
-        print(f"{jogadorAtual} VENCEU!!")
-        break
-
-#Condições de vitória Horizontal
-    if (jogo.tabuleiro['1'] == jogo.tabuleiro['2'] == jogo.tabuleiro['3'] == jogadorAtual or
-        jogo.tabuleiro['4'] == jogo.tabuleiro['5'] == jogo.tabuleiro['6'] == jogadorAtual or
-        jogo.tabuleiro['7'] == jogo.tabuleiro['8'] == jogo.tabuleiro['9'] == jogadorAtual):
-
-        print(f"{jogadorAtual} VENCEU!!")
-        break
-
-#Condições de vitoria Cruzado
-    if (jogo.tabuleiro['1'] == jogo.tabuleiro['5'] == jogo.tabuleiro['9'] == jogadorAtual or
-        jogo.tabuleiro['3'] == jogo.tabuleiro['5'] == jogo.tabuleiro['7'] == jogadorAtual):
-
-        print(f"{jogadorAtual} VENCEU!!")
-        break
-
-     # Verificador de Velha
-    if round == 8:
-            if not ((jogo.tabuleiro['1'] == jogo.tabuleiro['4'] == jogo.tabuleiro['7'] == jogadorAtual or
-                     jogo.tabuleiro['2'] == jogo.tabuleiro['5'] == jogo.tabuleiro['8'] == jogadorAtual or
-                     jogo.tabuleiro['3'] == jogo.tabuleiro['6'] == jogo.tabuleiro['9'] == jogadorAtual) or
-                    (jogo.tabuleiro['1'] == jogo.tabuleiro['2'] == jogo.tabuleiro['3'] == jogadorAtual or
-                     jogo.tabuleiro['4'] == jogo.tabuleiro['5'] == jogo.tabuleiro['6'] == jogadorAtual or
-                     jogo.tabuleiro['7'] == jogo.tabuleiro['8'] == jogo.tabuleiro['9'] == jogadorAtual) or
-                    (jogo.tabuleiro['1'] == jogo.tabuleiro['5'] == jogo.tabuleiro['9'] == jogadorAtual or
-                     jogo.tabuleiro['3'] == jogo.tabuleiro['5'] == jogo.tabuleiro['7'] == jogadorAtual)):
-                print("DEU VELHA")
-
-#Troca de Jogador
-    turno=turno+1
-    print(turno)
-    if jogadorAtual== "X":
-        jogadorAtual="O"
+    if jogadorAtual == "X":
+        jogadorAtual = "O"
     else:
-        jogadorAtual="X"
-    print(f"Agora é a vez do jogador '{jogadorAtual}'")
+        jogadorAtual = "X"
 
+    vencedor = False
+    turno = 2
 
+    for turnos in range(1, 9):
+        jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
+
+        while jogada < 1 or jogada > 9 or placar.tabuleiro[str(jogada)] != ' ':
+            if jogada < 1 or jogada > 9:
+                print("Escolha Inválida")
+                jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
+            else:
+                print("Slot Já Preenchido")
+                jogada = int(input(f"O jogador '{jogadorAtual}' Vai jogar em qual slot?\nExemplo de slot:(de 1 a 9): "))
+
+        jogo.tabuleiro[str(jogada)] = jogadorAtual
+        jogo.mostrar_tabuleiro()
+
+        if (placar.tabuleiro['1'] == placar.tabuleiro['4'] == placar.tabuleiro['7'] == jogadorAtual or
+            placar.tabuleiro['2'] == placar.tabuleiro['5'] == placar.tabuleiro['8'] == jogadorAtual or
+            placar.tabuleiro['3'] == placar.tabuleiro['6'] == placar.tabuleiro['9'] == jogadorAtual or
+            placar.tabuleiro['1'] == placar.tabuleiro['2'] == placar.tabuleiro['3'] == jogadorAtual or
+            placar.tabuleiro['4'] == placar.tabuleiro['5'] == placar.tabuleiro['6'] == jogadorAtual or
+            placar.tabuleiro['7'] == placar.tabuleiro['8'] == placar.tabuleiro['9'] == jogadorAtual or
+            placar.tabuleiro['1'] == placar.tabuleiro['5'] == placar.tabuleiro['9'] == jogadorAtual or
+            placar.tabuleiro['3'] == placar.tabuleiro['5'] == placar.tabuleiro['7'] == jogadorAtual):
+
+            print(f"{jogadorAtual} VENCEU!!")
+            vencedor = True
+            break
+
+        turno += 1
+        if jogadorAtual == "X":
+            jogadorAtual = "O"
+        else:
+            jogadorAtual = "X"
+
+    if turno == 9 and not vencedor:
+        print("DEU VELHA")
+
+    resp = input("Deseja jogar novamente? (Digite 'sim' para jogar novamente): ").lower()
+    if resp != "sim":
+        break
 
 
 
